60909833a27c35b8e251aa6a90fd018c8c843381
zhdfuture <49097626+zhdfuture@users.noreply.github.com>
2019-06-06 13:58:13 +0800

    Add files via upload

---
+++ MyRunnable.java
@@ -0,0 +1,16 @@
+package com.zh.R;
+
+public class MyRunnable implements  Runnable {
+    private final String title;
+
+    public MyRunnable(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void run()  {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(this.title + " " + i);
+        }
+    }
+}
