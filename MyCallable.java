a28cc02eb427b23249c0ec30915e174b9c13b075
zhdfuture <49097626+zhdfuture@users.noreply.github.com>
2019-06-08 07:55:16 +0800

    Add files via upload

---
+++ MyCallable.java
@@ -0,0 +1,15 @@
+package com.zh.R;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        Thread.sleep(1000);
+        return sum;
+    }
+}
